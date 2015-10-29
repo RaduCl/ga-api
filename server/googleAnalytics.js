c5912dc39485407ea9024ed4342aa5953cb7d401
RaduCl <raduclitan@gmail.com>
2015-10-29 16:12:28 +0200

    Dashboard prototype

--- server/googleAnalytics.js
+++ server/googleAnalytics.js
@@ -1,4 +1,8 @@
 var googleapis = require('googleapis');
+var json2csv = require('json2csv');
+
+var gaResults = [];
+
 var JWT = googleapis.auth.JWT;
 var analytics = googleapis.analytics('v3');
 
@@ -7,13 +11,13 @@ var SERVICE_ACCOUNT_KEY_FILE = __dirname + '/key.pem';
 
 
 var authClient = new JWT(
-    SERVICE_ACCOUNT_EMAIL,
-    SERVICE_ACCOUNT_KEY_FILE,
-    null,
-    ['https://www.googleapis.com/auth/analytics.readonly']
+        SERVICE_ACCOUNT_EMAIL,
+        SERVICE_ACCOUNT_KEY_FILE,
+        null,
+        ['https://www.googleapis.com/auth/analytics.readonly']
 );
 
-var googleAnalyticsData = authClient.authorize(function(err, tokens) {
+var getData = authClient.authorize(function(err, tokens) {
     if (err) {
         console.log(err);
         return;
@@ -22,13 +26,41 @@ var googleAnalyticsData = authClient.authorize(function(err, tokens) {
     analytics.data.ga.get({
         auth: authClient,
         'ids': 'ga:110116145',
-        'start-date': '2015-07-19',
-        'end-date': '2015-08-19',
-        'metrics': 'ga:visits'
+        'start-date': '2015-02-11',
+        'end-date': '2015-10-25',
+        // 'metrics': 'ga:visits, ga:sessionDuration',
+        'metrics': 'ga:visits',
+        'dimensions': 'ga:browser,ga:city,ga:sessionDurationBucket'
     }, function(err, result) {
-        console.log(err);
-        console.log(result);
+        if(err)
+            gaResults.push(err);
+        if(result){
+            return result;
+            // gaResults.push(result);
+            // exportCSV(gaResults);
+            //gaResults.push(result);
+            //gaResults = Object.assign({}, result);
+            //console.log(gaResults);
+            //console.log(JSON.stringify(gaResults.rows));
+        }
     });
 });
 
-module.exports = googleAnalyticsData;
+function exportCSV (gaResults) {
+    var fields = gaResults[0].columnHeaders.map(function(header){
+        return fields = header.name;
+    });
+    var data = gaResults[0].rows.map(function(row){
+        console.log("row.key is: " + fields[row.key]);
+    });
+    console.log("table header: " + fields);
+    console.log("rows: " + data);
+    json2csv({data:data, fields:fields}, function(err, csv){
+        if (err) console.log(err);
+        console.log("csv is here" + csv);
+    });
+}
+
+module.exports = getData;
+// module.exports = gaResults;
+// module.exports = exportCSV;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 var	express = require('express');
 var bodyParser = require('body-parser');
 var googleAnalytics = require('./googleAnalytics.js');
-
+// var googleAnalytics = require('./googleAnalytics.js');
+var json2csv = require('json2csv');
+var async = require('async');
 
 var app = express();
 
@@ -12,7 +14,8 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     //res.send("Hello!");
-    res.json(googleAnalytics);
+    console.log(googleAnalytics.getData);
+    res.send(googleAnalytics.getData);
 });
 
 app.listen(port, function(){
