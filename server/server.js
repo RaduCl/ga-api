e2e05614e34bd11924c7a5c90269d0a619e7a219
RaduCl <raduclitan@gmail.com>
2015-11-02 17:09:49 +0200

    partial auth implementation

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
-var	express = require('express')
-var stylus = require('stylus')
-var nib = require('nib')
-var bodyParser = require('body-parser')
-var googleAnalytics = require('./googleAnalytics.js')
-var json2csv = require('json2csv')
-//var path = require('path')
-
-//var html_dir = path.resolve('../dashboard/deploy/');
-//var css_dir = path.resolve('../dashboard/deploy/styles');
+var	express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
+var bodyParser = require('body-parser');
+var googleAnalytics = require('./googleAnalytics.js');
+var json2csv = require('json2csv');
+var session = require('express-session');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+
+
+// jade and stylus compile setup
 
 function compile(str, path){
     return stylus(str)
@@ -29,17 +30,65 @@ app.use(stylus.middleware(
 ))
 app.use(express.static(__dirname + '/public'))
 
+
+
+//middleware
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+
+//authentification
+
+//app.use(session({
+//    secret: '2C44-4D44-WppQ38S',
+//    resave: true,
+//    saveUninitialized: true
+//}));
+
+
+
+// Authentication and Authorization Middleware
+
+var auth = function(req, res, next) {
+    if (req.session && req.session.user === "amy")
+        return next();
+    else
+        return res.sendStatus(401);
+};
+
+
+//routes
+
 app.get('/', function(req, res){
+    if(req.user)
+        res.redirect('/dashboard')
+    else
+        res.redirect('login')
+})
+
+app.get('/login', function(req, res){
     //console.log(googleAnalytics.getData);
     //res.send(googleAnalytics.getData);
-    res.render('dashboard',
-        {title: 'Login'}
+
+    //if user is logged in redirect
+    if(req.user)
+        res.redirect('/dashboard')
+    else
+        res.render('login',
+             {title: 'Login'}
     );
 });
 
+app.post('/login', auth)
+
+app.get('/dashboard', function(req, res){
+    res.render('dashboard',
+        {title: 'Yamaha Dashboard'}
+    )
+})
+
+// init server
 app.listen(port, function(){
     console.log('App is now running on PORT: ' + port);
 });
