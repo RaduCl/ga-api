bc951147cdb67b04ed120486e19a7c03f7567d97
RaduCl <raduclitan@gmail.com>
2015-11-02 15:02:54 +0200

    moved view files under server folder

--- server/server.js
+++ server/server.js
@@ -1,21 +1,43 @@
-var	express = require('express');
-var bodyParser = require('body-parser');
-var googleAnalytics = require('./googleAnalytics.js');
-// var googleAnalytics = require('./googleAnalytics.js');
-var json2csv = require('json2csv');
-var async = require('async');
+var	express = require('express')
+var stylus = require('stylus')
+var nib = require('nib')
+var bodyParser = require('body-parser')
+var googleAnalytics = require('./googleAnalytics.js')
+var json2csv = require('json2csv')
+//var path = require('path')
+
+//var html_dir = path.resolve('../dashboard/deploy/');
+//var css_dir = path.resolve('../dashboard/deploy/styles');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+function compile(str, path){
+    return stylus(str)
+        .set('filename', path)
+        .use(nib())
+}
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+//app.use(express.logger('dev'))
+app.use(stylus.middleware(
+    { src: __dirname + '/public'
+        , compile: compile
+    }
+))
+app.use(express.static(__dirname + '/public'))
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-    //res.send("Hello!");
-    console.log(googleAnalytics.getData);
-    res.send(googleAnalytics.getData);
+    //console.log(googleAnalytics.getData);
+    //res.send(googleAnalytics.getData);
+    res.render('dashboard',
+        {title: 'Login'}
+    );
 });
 
 app.listen(port, function(){
