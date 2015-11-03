4a40d1704c394f373c4ae7d3950b84f30477d50b
RaduCl <raduclitan@gmail.com>
2015-11-03 17:03:05 +0200

    unstaged folders

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
-var	express = require('express');
 var stylus = require('stylus');
 var nib = require('nib');
-var bodyParser = require('body-parser');
 //var googleAnalytics = require('./googleAnalytics.js');
 var json2csv = require('json2csv');
-var session = require('express-session');
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
-
+var	express = require('express');
 var app = express();
-
 var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var Strategy = require('passport-local').Strategy;
+var dbConfig = require('./config/db.js');
+mongoose.connect(dbConfig.url);
+require('./config/passport')(passport); // pass passport for configuration
+//var routes = require('./routes/routes');
 
 
 
@@ -38,24 +44,29 @@ app.use(express.static(__dirname + '/public'))
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-
-//authentification
+// Configuring Passport
 
 app.use(session({
     secret: '2C23-4D14-WpPQ38S',
     resave: false,
     saveUninitialized: false
 }));
+// Initialize Passport and restore authentication state, if any, from the session.
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-//TODO implement with db and hash+salt in production
-var users = {
-    'T3stUser1': '!d3fau1tPa551',
-    'T3stUser2': '!d3fau1tPa552'
-}
+//routes
+require('./routes/routes.js')(app, passport);
 
-//passport.use('login', new Strategy(
-//    function(username, password, done) {
+
+//passport.use('login-local', new Strategy(
+//    {
+//        passReqToCallback: true
+//    },
+//    function(req, username, password, done) {
 //        console.log('username: ',username)
 //        if (!users[username] || users[username] != password) {
 //            return done(null, false);
@@ -63,25 +74,7 @@ var users = {
 //        return done(null, { username : username });
 //    }
 //));
-
-passport.use('login-local', new Strategy(
-    {
-        passReqToCallback: true
-    },
-    function(req, username, password, done) {
-        console.log('username: ',username)
-        if (!users[username] || users[username] != password) {
-            return done(null, false);
-        }
-        return done(null, { username : username });
-    }
-));
-
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
+;
 
 // Authentication and Authorization Middleware
 
@@ -96,48 +89,48 @@ app.use(passport.session());
 
 //routes
 
-app.get('/', function(req, res){
-    if(req.user)
-        res.redirect('/dashboard')
-    else
-        res.redirect('login')
-})
-
-app.get('/login', function(req, res){
-    //res.send(googleAnalytics.getData);
-
-    //if user is logged in redirect
-    console.log('get/login')
-    //if(req.user)
-    //    res.redirect('/dashboard')
-    //else
-        res.render('login',
-             {title: 'Login'}
-    );
-});
-
-app.post('/login',
-    //passport.authenticate('local', {
-    //    failureRedirect: '/login',
-    //    succesRedirect: '/dashboard'
-    //})
-
-    passport.authenticate('login-local', function(err, user){
-        console.log('user: '+user);
-        if(err) console.log(err);
-        if(!user) {return res.redirect('/login')}
-        res.end('Authenticated!')
-    })(req, res)
-    //function(req, res){
-    //    res.redirect('/dashboard')
-    //}
-)
-
-app.get('/dashboard', function(req, res){
-    res.render('dashboard',
-        {title: 'Yamaha Dashboard'}
-    )
-})
+//app.get('/', function(req, res){
+//    if(req.user)
+//        res.redirect('/dashboard')
+//    else
+//        res.redirect('login')
+//})
+//
+//app.get('/login', function(req, res){
+//    //res.send(googleAnalytics.getData);
+//
+//    //if user is logged in redirect
+//    console.log('get/login')
+//    //if(req.user)
+//    //    res.redirect('/dashboard')
+//    //else
+//        res.render('login',
+//             {title: 'Login'}
+//    );
+//});
+//
+//app.post('/login',
+//    //passport.authenticate('local', {
+//    //    failureRedirect: '/login',
+//    //    succesRedirect: '/dashboard'
+//    //})
+//
+//    //passport.authenticate('login-local', function(err, user){
+//    //    console.log('user: '+user);
+//    //    if(err) console.log(err);
+//    //    if(!user) {return res.redirect('/login')}
+//    //    res.end('Authenticated!')
+//    //})(req, res)
+//    function(req, res){
+//        res.redirect('/dashboard')
+//    }
+//)
+//
+//app.get('/dashboard', function(req, res){
+//    res.render('dashboard',
+//        {title: 'Yamaha Dashboard'}
+//    )
+//})
 
 // init server
 app.listen(port, function(){
