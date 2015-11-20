66f1a2e4a23b87f917c220a807d8011d03bb173e
drb1988 <bogdan.drb@bigcityapps.ro>
2015-11-20 14:19:22 +0200

    Added new querries

--- analytics/AnalyticsQueries.js
+++ analytics/AnalyticsQueries.js
@@ -12,87 +12,138 @@ var queryController = function(){
         if(mm<10){
             mm='0'+mm
         }
+        console.log("azi "+today);
         return today = yyyy+'-'+mm+'-'+dd;
     }
 
-    function setWeekAgo(){
-        var oneWeekAgo = new Date();
-        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-        var dd = oneWeekAgo.getDate();
-        var mm = oneWeekAgo.getMonth()+1; //January is 0!
-        var yyyy = oneWeekAgo.getFullYear();
-        if(dd<10){
-            dd='0'+dd
-        }
-        if(mm<10){
-            mm='0'+mm
-        }
-        return oneWeekAgo = yyyy+'-'+mm+'-'+dd;
-    }
-
     function setMonthAgo(){
-        var monthAgo = new Date();
-        monthAgo.setMonth(monthAgo.getMonth() - 1);
-        var dd = monthAgo.getDate();
-        var mm = monthAgo.getMonth()+1; //January is 0!
-        var yyyy = monthAgo.getFullYear();
+        var today = new Date()-30;
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
         if(dd<10){
             dd='0'+dd
         }
         if(mm<10){
             mm='0'+mm
         }
-        return monthAgo = yyyy+'-'+mm+'-'+dd;
+        console.log("azi "+today);
+        return today = yyyy+'-'+mm+'-'+dd;
     }
 
-    var endDate = setToday();
-    var startDate = setWeekAgo();
-    //var startDate = setMonthAgo()
+    var endDate = setToday()
 
-    // Query objects used in getData()
+
+    //var monthAgo = new Date(today)-30;
+
+
     var queries = {
 
         osQuery: {
-            'start-date': startDate,
+            'start-date': '2015-10-11',
             'end-date': endDate,
             'metrics': 'ga:visits',
             'dimensions': 'ga:operatingSystemVersion, ga:operatingSystem'
         },
 
-        countryVisitsQuery: {
-            'start-date': startDate,
+        AndroidcountryVisitsQuery: {
+            'start-date': '2015-09-11',
             'end-date': endDate,
             'metrics': 'ga:visits',
-            'dimensions': 'ga:country'
+            'filters': 'ga:operatingSystem==Android',
+            'dimensions': 'ga:country',
+            'sort': '-ga:visits'
+        },
+
+        iOScountryVisitsQuery: {
+            'start-date': '2015-09-11',
+            'end-date': endDate,
+            'metrics': 'ga:visits',
+            'filters': 'ga:operatingSystem==iOS',
+            'dimensions': 'ga:country',
+            'sort': '-ga:visits'
         },
 
         dailyUsersQuery: {
-            'start-date': startDate,
+            'start-date': '2015-09-11',
             'end-date': endDate,
             'metrics': 'ga:1dayUsers',
             'dimensions': 'ga:date'
         },
 
         frequencyQuery: {
-            'start-date': startDate,
+            'start-date': '2015-09-11',
             'end-date': endDate,
             'metrics': 'ga:sessions',
             'dimensions': 'ga:daysSinceLastSession'
         },
 
-        loyaltyQuery: {
-            'start-date': startDate,
+        iOSLoyaltyQuery: {
+            'start-date': '2015-10-22',
             'end-date': endDate,
             'metrics': 'ga:sessions',
-            'dimensions': 'ga:sessionCount'
+            'dimensions': 'ga:sessionCount',
+            'filters': 'ga:operatingSystem==iOS'
+        },
+
+        AndroidLoyaltyQuery: {
+            'start-date': '2015-10-22',
+            'end-date': endDate,
+            'metrics': 'ga:sessions',
+            'dimensions': 'ga:sessionCount',
+            'filters': 'ga:operatingSystem==Android'
         },
 
         visitorTypesQuery: {
-            'start-date': startDate,
+            'start-date': '2015-10-22',
             'end-date': endDate,
             'metrics': 'ga:visits',
             'dimensions': 'ga:userType'
+        },
+
+        popularBikesQuery: {
+            'start-date': '2015-10-20',
+            'end-date': endDate,
+            'metrics': 'ga:totalEvents',
+            'filters': 'ga:eventCategory==Bike', //ga:operatingSystem==iOS',
+            'dimensions': 'ga:eventLabel',
+            'sort': '-ga:totalEvents'
+        },
+
+        popularPartsQuery: {
+            'start-date': '2015-10-20',
+            'end-date': endDate,
+            'metrics': 'ga:totalEvents',
+            'filters': 'ga:eventCategory==Part', //ga:operatingSystem==iOS',
+            'dimensions': 'ga:eventLabel',
+            'sort': '-ga:totalEvents'
+        },
+
+        sharesQuery: {
+            'start-date': '2015-10-20',
+            'end-date': endDate,
+            'metrics': 'ga:totalEvents',
+            'filters': 'ga:eventCategory==Share', //ga:operatingSystem==iOS',
+        },
+
+        iOSExitQuery: {
+            'start-date': '2015-10-20',
+            'end-date': endDate,
+            'metrics': 'ga:visits',
+            'dimensions': 'ga:exitScreenName',
+            'filters': 'ga:operatingSystem==iOS',
+            'sort': '-ga:visits'
+        },
+
+        AndroidExitQuery: {
+            'start-date': '2015-10-20',
+            'end-date': endDate,
+            'metrics': 'ga:visits',
+            'dimensions': 'ga:exitScreenName',
+            'filters': 'ga:operatingSystem==Android',
+            'sort': '-ga:visits'
         }
+
     }
 
     return queries;
