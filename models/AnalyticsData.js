ea6021c9bc8345c0c6078485844e631cdecf2efe
RaduCl <raduclitan@gmail.com>
2015-11-24 17:13:59 +0200

    set interval 200 ms

--- models/AnalyticsData.js
+++ models/AnalyticsData.js
@@ -1,9 +1,9 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema();
-var gaData = new Schema({
-    date: {type: Date},
-    period: {type: String},
-    queries:{type: String}
-})
-
-module.exports = mongoose.model('gaData', gaData);
+//var mongoose = require('mongoose');
+//var Schema = mongoose.Schema();
+//var gaData = new Schema({
+//    date: {type: Date},
+//    period: {type: String},
+//    queries:{type: String}
+//})
+//
+//module.exports = mongoose.model('gaData', gaData);
