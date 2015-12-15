228815960d45c82d1bdaa2c21fbcd9d4c5f46a5a
drb1988 <bogdan.drb@bigcityapps.ro>
2015-12-15 12:08:01 +0200

    Worked on refactoring the Apple data// to do: sort by country

--- analytics/apple.js
+++ analytics/apple.js
@@ -1,147 +1,45 @@
 var itc = require("itunesconnect");
+var prettyjson = require('prettyjson');
 var Report = itc.Report;
 
 // Connect to iTunes
 var itunes = new itc.Connect('connectstats@indg.com', 'Dsc729max!');
 
 // Simple ranked report
+var countryKeys = {
+    'Italy' : 143450,
+    'Spain' : 143454,
+    'France' : 143442,
+    'Germany' : 143443,
+    'UK' : 143444
+}
 
-itunes.request(Report.ranked().time(1, 'weeks').location(143450), function (error, result) {
+
+
+    var getAppStoreDataByCountry = function(country, callback)    { 
+        console.log(keys);
+        itunes.request(Report.ranked().time(1, 'weeks').location(country), function (error, result) {
         var finalRes = {}
+
         if(error) return error
         if (result){
-			finalRes.downloads = []
-			finalRes.previousDownloads = []
-			finalRes.delta = []
-            finalRes.downloads[0] = result[0].units;
-			finalRes.downloads[1] = result[1].units;
-			finalRes.downloads[2] = result[2].units;
-            itunes.request(Report.ranked().time(2, 'weeks').location(143450), function (error, result) {
+            result.map(function(app){
+                var rObj = {}
+                rObj['downloads'] = app.units
+                finalRes[app.title.replace(/ /gi, '')] = rObj
+            })
+            itunes.request(Report.ranked().time(2, 'weeks').location(country), function (error, result) {
                 if(error) return error
                 if(result){
-                    finalRes.previousDownloads[0] = result[0].units - finalRes.downloads[0];
-					finalRes.previousDownloads[1] = result[1].units - finalRes.downloads[1];
-					finalRes.previousDownloads[2] = result[2].units - finalRes.downloads[2];
-                    finalRes.delta[0] = finalRes.downloads[0] - finalRes.previousDownloads[0];
-					finalRes.delta[1] = finalRes.downloads[1] - finalRes.previousDownloads[1];
-					finalRes.delta[2] = finalRes.downloads[2] - finalRes.previousDownloads[2];
-               //     finalRes.deltaPercentage = getDeltaPercentage(finalRes.downloads[0], finalRes.previousDownloads[0]);
-					console.log("Italy");
-					console.log(finalRes);
-                  //  if(callback) callback(finalRes)
+                    result.map(function(app){
+                        finalRes[app.title.replace(/ /gi, '')].previousDownloads = app.units - finalRes[app.title.replace(/ /gi, '')].downloads
+                        finalRes[app.title.replace(/ /gi, '')].delta = finalRes[app.title.replace(/ /gi, '')].downloads - finalRes[app.title.replace(/ /gi, '')].previousDownloads
+                    //    finalRes[app.title.replace(/ /gi, '')].deltaPercentage = getDeltaPercentage(finalRes[app.title.replace(/ /gi, '')].downloads, finalRes[app.title.replace(/ /gi, '')].previousDownloads )
+                    })
+                    console.log(prettyjson.render(finalRes));
+                //    if(callback) callback(finalRes)
                 }
             });
         }
     });
-	
-	itunes.request(Report.ranked().time(1, 'weeks').location(143454), function (error, result) {
-        var finalRes = {}
-        if(error) return error
-        if (result){
-			finalRes.downloads = []
-			finalRes.previousDownloads = []
-			finalRes.delta = []
-            finalRes.downloads[0] = result[0].units;
-			finalRes.downloads[1] = result[1].units;
-			finalRes.downloads[2] = result[2].units;
-            itunes.request(Report.ranked().time(2, 'weeks').location(143454), function (error, result) {
-                if(error) return error
-                if(result){
-                    finalRes.previousDownloads[0] = result[0].units - finalRes.downloads[0];
-					finalRes.previousDownloads[1] = result[1].units - finalRes.downloads[1];
-					finalRes.previousDownloads[2] = result[2].units - finalRes.downloads[2];
-                    finalRes.delta[0] = finalRes.downloads[0] - finalRes.previousDownloads[0];
-					finalRes.delta[1] = finalRes.downloads[1] - finalRes.previousDownloads[1];
-					finalRes.delta[2] = finalRes.downloads[2] - finalRes.previousDownloads[2];
-               //     finalRes.deltaPercentage = getDeltaPercentage(finalRes.downloads[0], finalRes.previousDownloads[0]);
-					console.log("Spain");
-					console.log(finalRes);
-                  //  if(callback) callback(finalRes)
-                }
-            });
-        }
-    });
-	
-	itunes.request(Report.ranked().time(1, 'weeks').location(143442), function (error, result) {
-        var finalRes = {}
-        if(error) return error
-        if (result){
-			finalRes.downloads = []
-			finalRes.previousDownloads = []
-			finalRes.delta = []
-            finalRes.downloads[0] = result[0].units;
-			finalRes.downloads[1] = result[1].units;
-			finalRes.downloads[2] = result[2].units;
-            itunes.request(Report.ranked().time(2, 'weeks').location(143442), function (error, result) {
-                if(error) return error
-                if(result){
-                    finalRes.previousDownloads[0] = result[0].units - finalRes.downloads[0];
-					finalRes.previousDownloads[1] = result[1].units - finalRes.downloads[1];
-					finalRes.previousDownloads[2] = result[2].units - finalRes.downloads[2];
-                    finalRes.delta[0] = finalRes.downloads[0] - finalRes.previousDownloads[0];
-					finalRes.delta[1] = finalRes.downloads[1] - finalRes.previousDownloads[1];
-					finalRes.delta[2] = finalRes.downloads[2] - finalRes.previousDownloads[2];
-               //     finalRes.deltaPercentage = getDeltaPercentage(finalRes.downloads[0], finalRes.previousDownloads[0]);
-			   		console.log("France");
-					console.log(finalRes);
-                  //  if(callback) callback(finalRes)
-                }
-            });
-        }
-    });
-	
-		itunes.request(Report.ranked().time(1, 'weeks').location(143443), function (error, result) {
-        var finalRes = {}
-        if(error) return error
-        if (result){
-			finalRes.downloads = []
-			finalRes.previousDownloads = []
-			finalRes.delta = []
-            finalRes.downloads[0] = result[0].units;
-			finalRes.downloads[1] = result[1].units;
-			finalRes.downloads[2] = result[2].units;
-            itunes.request(Report.ranked().time(2, 'weeks').location(143443), function (error, result) {
-                if(error) return error
-                if(result){
-                    finalRes.previousDownloads[0] = result[0].units - finalRes.downloads[0];
-					finalRes.previousDownloads[1] = result[1].units - finalRes.downloads[1];
-					finalRes.previousDownloads[2] = result[2].units - finalRes.downloads[2];
-                    finalRes.delta[0] = finalRes.downloads[0] - finalRes.previousDownloads[0];
-					finalRes.delta[1] = finalRes.downloads[1] - finalRes.previousDownloads[1];
-					finalRes.delta[2] = finalRes.downloads[2] - finalRes.previousDownloads[2];
-               //     finalRes.deltaPercentage = getDeltaPercentage(finalRes.downloads[0], finalRes.previousDownloads[0]);
-			  		console.log("Germany");
-					console.log(finalRes);
-                  //  if(callback) callback(finalRes)
-                }
-            });
-        }
-    });
-	
-		itunes.request(Report.ranked().time(1, 'weeks').location(143444), function (error, result) {
-        var finalRes = {}
-        if(error) return error
-        if (result){
-			finalRes.downloads = []
-			finalRes.previousDownloads = []
-			finalRes.delta = []
-            finalRes.downloads[0] = result[0].units;
-			finalRes.downloads[1] = result[1].units;
-			finalRes.downloads[2] = result[2].units;
-            itunes.request(Report.ranked().time(2, 'weeks').location(143444), function (error, result) {
-                if(error) return error
-                if(result){
-                    finalRes.previousDownloads[0] = result[0].units - finalRes.downloads[0];
-					finalRes.previousDownloads[1] = result[1].units - finalRes.downloads[1];
-					finalRes.previousDownloads[2] = result[2].units - finalRes.downloads[2];
-                    finalRes.delta[0] = finalRes.downloads[0] - finalRes.previousDownloads[0];
-					finalRes.delta[1] = finalRes.downloads[1] - finalRes.previousDownloads[1];
-					finalRes.delta[2] = finalRes.downloads[2] - finalRes.previousDownloads[2];
-               //     finalRes.deltaPercentage = getDeltaPercentage(finalRes.downloads[0], finalRes.previousDownloads[0]);
-					console.log("UK");
-					console.log(finalRes);
-                  //  if(callback) callback(finalRes)
-                }
-            });
-        }
-    });
+}
