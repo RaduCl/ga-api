df9bd4f3f13cd5a732698061c1cfcd16a4de0668
RaduCl <raduclitan@gmail.com>
2015-12-16 01:28:07 +0200

    getAppStoreByCountry update

--- analytics/AppStoreAnalytics.js
+++ analytics/AppStoreAnalytics.js
@@ -1,128 +1,83 @@
 var itc = require("itunesconnect");
 var Report = itc.Report;
 var Q = require('q');
+var prettyjson = require('prettyjson')
 // Connect to iTunes
 var itunes = new itc.Connect('connectstats@indg.com', 'Dsc729max!');
-var finalRes = {
-    week : 0,
-    month: 0,
-    previousWeek: 0,
-    previousMonth: 0,
-    monthDelta: 0,
-    weekDelta: 0
-}
+
 
 //helper method for getting deference between current and previous time interval in percents
 var getDeltaPercentage = function(currentResult, prevResult){
     if((prevResult)){
         return Math.floor((currentResult-prevResult)/prevResult*100);
     } else return('-')};
-//helper method to format by country respone object
-var formatByCountry = function(result, callback){
-    var res = {}
-    for(var i=0; i< result.length; i++){
-        res[result[i].title] = result[i].units
-    }
-    callback(res)
-}
 
-var countryKeys = {
-    'Italy' : 143450,
-    'Spain' : 143454,
-    'France' : 143442,
-    'Germany' : 143443,
-    'UK' : 143444
-}
-
-var getAppStoreDataByCountry = function(interval, callback){
+var getAppStoreDataByCountryTemp = function(interval, country, callback){
     var finalRes = {}
-    var tempRes = {}
-    for(var country in countryKeys){
-
-        itunes.request(Report.ranked().time(1, interval+"s").location(countryKeys[country]), function (error, result) {
-            if(error) return error
-            if(result){
-                result.map(function(app){
-                    var rObj = {}
-                    rObj['downloads'] = app.units
-                    tempRes[app.title.replace(/ /gi, '')] = rObj
-                    //console.log(rObj);
-                    return rObj
-                })
-            }
-        })
+    var countryKeys = {
+        'Italy' : 143450,
+        'Spain' : 143454,
+        'France' : 143442,
+        'Germany' : 143443,
+        'UK' : 143444
     }
-
-
-        //itunes.request(Report.ranked().time(1, interval+"s").location(143450), function (error, result) {
-    //    var finalRes = []
-    //    var tempRes = {}
-    //    if(error) return error
-    //    if (result){
-    //        console.log(result);
-    //        tempRes
-    //        //formatByCountry(result,function(res){
-    //        //    tempRes.res1=res
-    //        //})
-    //        console.log(tempRes);
-    //        itunes.request(Report.ranked().time(2, interval+'s').location(143450), function (error, result) {
-    //            if(error) return error
-    //            formatByCountry(result,function(res){
-    //                tempRes.res2=res
-    //
-    //                var countries = Object.keys(tempRes.res1).slice(0,5)
-    //                finalRes = countries.map(function(country){
-    //                    var countryObj={}
-    //                    countryObj.name=country
-    //                    countryObj.downloads = tempRes.res1[country]
-    //                    countryObj.previousDownloads = tempRes.res2[country]-tempRes.res1[country]
-    //                    countryObj.delta = countryObj.downloads - countryObj.previousDownloads
-    //                    countryObj.deltaPercentage = getDeltaPercentage(countryObj.downloads, countryObj.previousDownloads)
-    //                    return countryObj
-    //                })
-    //
-    //                //format othercountries downloads stats
-    //                var otherCountries = {}
-    //
-    //                otherCountries.name = 'Other countries'
-    //                otherCountries.downloads = Object.keys(tempRes.res1).slice(6).map(function(country){
-    //                    return tempRes.res1[country]
-    //                }).reduce(function(prev, current){
-    //                    return prev+current
-    //                },0)
-    //                otherCountries.previousDownloads = Object.keys(tempRes.res1).slice(6).map(function(country){
-    //                    return tempRes.res2[country]
-    //                }).reduce(function(prev, current){
-    //                    return prev+current
-    //                },0);
-    //                otherCountries.previousDownloads = otherCountries.previousDownloads - otherCountries.downloads;
-    //                otherCountries.delta = otherCountries.downloads - otherCountries.previousDownloads
-    //                otherCountries.deltaPercentage = getDeltaPercentage(otherCountries.downloads, otherCountries.previousDownloads)
-    //
-    //                //format total downlods stats
-    //                var totalDl = {}
-    //
-    //                totalDl.name = 'Total Downloads'
-    //                totalDl.downloads = Object.keys(tempRes.res1).map(function(country){
-    //                    return tempRes.res1[country]
-    //                }).reduce(function(prev, current){
-    //                    return prev+current
-    //                },0)
-    //                totalDl.previousDownloads = Object.keys(tempRes.res1).map(function(country){
-    //                    return tempRes.res2[country]
-    //                }).reduce(function(prev, current){
-    //                    return prev+current
-    //                },0)
-    //                totalDl.previousDownloads = totalDl.previousDownloads - totalDl.downloads
-    //                totalDl.delta = totalDl.downloads - totalDl.previousDownloads
-    //                totalDl.deltaPercentage = getDeltaPercentage(totalDl.downloads, totalDl.previousDownloads)
-    //                finalRes.push(otherCountries, totalDl);
-    //                if(callback) callback(finalRes)
-    //            })
-    //        })
-    //    }
-    //});
+    var country = country
+    itunes.request(Report.ranked().time(1, interval+"s").location(countryKeys[country]), function (error, result) {
+        if(error) return error
+        if(result){
+            //console.log(result);
+            var x = result.map(function(app){
+                var rObj = {}
+                rObj['downloads'] = app.units
+                return finalRes[app.title.replace(/ /gi, '')] = rObj
+            })
+            //console.log(finalRes);
+            //console.log(x);
+            itunes.request(Report.ranked().time(2, interval+"s").location(countryKeys[country]), function (error, result) {
+                if(error) return error
+                if(result){
+                    //console.log(result);
+                    var data = result.map(function(app){
+                        console.log(finalRes[app.title.replace(/ /gi, '')]);
+                        finalRes[app.title.replace(/ /gi, '')].previousDownloads = app.units - finalRes[app.title.replace(/ /gi, '')].downloads
+                        finalRes[app.title.replace(/ /gi, '')].delta = finalRes[app.title.replace(/ /gi, '')].downloads - finalRes[app.title.replace(/ /gi, '')].previousDownloads
+                        finalRes[app.title.replace(/ /gi, '')].deltaPercentage = getDeltaPercentage(finalRes[app.title.replace(/ /gi, '')].downloads, finalRes[app.title.replace(/ /gi, '')].previousDownloads )
+                        finalRes[app.title.replace(/ /gi, '')].country = country
+                        return finalRes
+                    })
+                    //console.log(data);
+                    if(callback) callback(data)
+                }
+            })
+        }
+    })
 }
+var getAppStoreDataByCountry = function(interval, callback){
+    var rObj = {}
+    getAppStoreDataByCountryTemp(interval, 'Spain', function(data){
+        console.log('\n'+data);
+        rObj.Spain = data
+        getAppStoreDataByCountryTemp(interval, 'Italy', function(data){
+            console.log('\n'+prettyjson.render(data));
+            rObj.Italy = data
+            getAppStoreDataByCountryTemp(interval, 'France', function(data){
+                console.log('\n'+data);
+                rObj.France = data
+                getAppStoreDataByCountryTemp(interval, 'Germany', function(data){
+                    console.log('\n'+data);
+                    rObj.Germany = data
+                    getAppStoreDataByCountryTemp(interval, 'UK'), function(data){
+                        console.log('\n'+prettyjson.render(data));
+                        rObj.UK = data
+                        //console.log(prettyjson.render(rObj));
+                        if(callback) callback(rObj)
+                    }
+                })
+            })
+        })
+    })
+}
+//getAppStoreDataByCountry('week', 'Spain')
 
 var getAppStoreData = function(interval, callback){
 
