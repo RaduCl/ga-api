8e7f7442c9c71cf56fc9c2808c0656b663dc6013
drb1988 <bogdan.drb@bigcityapps.ro>
2016-01-18 14:40:06 +0200

    fixed headers

--- public/js/app.js
+++ public/js/app.js
@@ -137,7 +137,7 @@ function initialize() {
         var country = ''
 
         var opts = {
-            headers:true,
+            headers:false,
             sheetid:"Sheet1",
             column: {style:{Font:{Bold:"1"}}},
             rows: {1:{style:{Font:{Color:"#FF0000"}}}},
