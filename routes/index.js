cbfde36efc840a0aaaec9c075157f3d71178e101
RaduCl <raduclitan@gmail.com>
2016-01-27 10:43:28 +0200

    removed unused routes and linted code

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,34 @@ var router = express.Router();
 var Q = require('q');
 var dbConfig = require('../db');
 var db = require('mongoskin').db(dbConfig.url);//mongo driver for loose data manipulation
-var dbSeedData = require('../analytics/bidbFeeder')
+var dbSeedData = require('../analytics/bidbFeeder');
 
 
 
 //////helper function//////
 var getYesterday = function(){
 	var today = new Date();
-	var currDate = today.getDate()
-	today.setDate(currDate-1)
-	var yesterday = today.toString()
-	return yesterday.slice(0, 15)
-}
+	var currDate = today.getDate();
+	today.setDate(currDate-1);
+	var yesterday = today.toString();
+	return yesterday.slice(0, 15);
+};
 ///////////////////////////
 
 var isAuthenticated = function (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler 
+	// if user is authenticated in the session, call the next() to call the next request handler
 	// Passport adds this method to request object. A middleware is allowed to add properties to
 	// request and response objects
 	if (req.isAuthenticated())
 		return next();
 	// if the user is not authenticated then redirect him to the login page
 	res.redirect('/');
-}
+};
 
 module.exports = function(passport){
 
 	router.get('/', function(req, res) {
-		res.redirect('/login')
+		res.redirect('/login');
 	});
 
     /* GET login page. */
@@ -40,13 +40,13 @@ module.exports = function(passport){
             title: 'Login',
             message: req.flash('message')
         });
-    })
+    });
 
 	/* Handle Login POST */
 	router.post('/login', passport.authenticate('login', {
 		successRedirect: '/dashboard',
 		failureRedirect: '/',
-		failureFlash : true  
+		failureFlash : true
 	}));
 
 	// /* GET Registration Page */
@@ -54,11 +54,11 @@ module.exports = function(passport){
 	// 	res.render('register',{message: req.flash('message')});
 	// });
 
-	//  Handle Registration POST 
+	//  Handle Registration POST
 	// router.post('/signup', passport.authenticate('signup', {
 	// 	successRedirect: '/dashboard',
 	// 	failureRedirect: '/signup',
-	// 	failureFlash : true  
+	// 	failureFlash : true
 	// }));
 
 	/* GET Home Page */
@@ -87,12 +87,12 @@ module.exports = function(passport){
 	router.get('/ga-data', function(req, res){
 
 		//get the parametrized partial query objects
-		var queries = require('../analytics/AnalyticsQueries')
+		var queries = require('../analytics/AnalyticsQueries');
 		//get the google-analytics authentication module
 		var googleAnalytics = require('../analytics/googleAnalytics2').getAllData;
 
 
-		var data = {}
+		var data = {};
 		var i=0;
 		var queryLength = Object.keys(queries).length;
 
@@ -100,73 +100,72 @@ module.exports = function(passport){
 		for(var q in queries){
 			googleAnalytics(function (err, result, queryKey){
 
-				if(err) return console.log(err)
+				if(err) return console.log(err);
 				if(result){
-
 					//rename object key with the query key
-					data[queryKey] = result.rows
+					data[queryKey] = result.rows;
 					i++;
 				}
 				if(i==queryLength)
 				{
-					console.log(data)
-					res.send(data)
+					console.log(data);
+					res.send(data);
 				}
-			}, queries[q], q)
+			}, queries[q], q);
 		}
 	});
 
 	router.get('/appstore-data', function(req, res){
-		var AppStoreData = require('../analytics/AppStoreAnalytics')
+		var AppStoreData = require('../analytics/AppStoreAnalytics');
 		AppStoreData('week', function(result){
 			//if(err) console.log(err)
 			if(result){
-				res.json(result)
+				res.json(result);
 			}
 			//console.log("res is: " + res)
-		})
-	})
+		});
+	});
 
 	/* GET  all analytics data from DB for MyGarage stats table */
 	router.get('/mygarage-data', isAuthenticated, function(req, res){
-		var today = Date().slice(0, 15)
+		var today = Date().slice(0, 15);
 		var yesterday = getYesterday();
 		var date = '';
-		var allResults = {}
+		var allResults = {};
 
 		//check if current date Data is available
 		db.collection(dbConfig.collection).findOne({timeInterval: 'week', createDate: today}, function(e, results) {
-			if (e) return next(e)
+			if (e) return next(e);
 			results ? date = today : date = yesterday
 			console.log('Getting data from: ',date);
 			db.collection(dbConfig.collection).findOne({timeInterval: 'week', createDate: date}, function(e, results){
-				if(e) return next(e)
+				if(e) return next(e);
 				if(results) {
-					allResults.weekResults = results
+					allResults.weekResults = results;
 					db.collection(dbConfig.collection).findOne({timeInterval: 'month', createDate: date}, function (e, results) {
-						if (e) return next(e)
+						if (e) return next(e);
 						if (results) {
-							allResults.monthResults = results
+							allResults.monthResults = results;
 							db.collection(dbConfig.collection).findOne({timeInterval: 'year', createDate: date}, function(e, results){
-								allResults.yearResults = results
-								res.send(allResults)
-							})
+								allResults.yearResults = results;
+								res.send(allResults);
+							});
 						}
-					})
+					});
 				}
-			})
-		})
-	})
+			});
+		});
+	});
 
 
 	/* GET  google all analytics data from DB */
 	////TODO in production secure this route by using isAuthenticated param
 	router.get('/mongo-data', isAuthenticated, function(req, res){
-		var today = Date().slice(0, 15)
+		var today = Date().slice(0, 15);
 		db.collection(dbConfig.collection).findOne({timeInterval: 'week', createDate: today}, function(e, results){
-			if(e) return next(e)
-			res.send(results.Data)
-		})
+			if(e) return next(e);
+			res.send(results.Data);
+		});
 		//res.send('data from mongo')
 	});
 
@@ -175,181 +174,52 @@ module.exports = function(passport){
 		/* GET  filtered by time interval all analytics data from DB */
 	//TODO in production secure this route by using isAuthenticated param
 	router.get('/mongo-data/:timeInterval', function(req, res){
-		var today = Date().slice(0, 15)
-		var yesterday = getYesterday()
+		var today = Date().slice(0, 15);
+		var yesterday = getYesterday();
 		console.log(today);
 		db.collection(dbConfig.collection).findOne({timeInterval: req.params.timeInterval, createDate: today}, function(e, results, next){
-			if(e) res.status(500).send(e)
+			if(e) res.status(500).send(e);
 			if(results){
-				res.send(results.Data)
+				res.send(results.Data);
 			} else {
 				//check if ingest is currently running
 				if (ingest){
 						//res.status(500).send('Ingest curently running. Please return when done.')
 						//retrieve previous available data to present
 						db.collection(dbConfig.collection).findOne({timeInterval: req.params.timeInterval, createDate: yesterday}, function(e, results, next){
-							if(e) res.status(500).send(e)
+							if(e) res.status(500).send(e);
 							if(results){
-								res.send(results.Data)
+								res.send(results.Data);
 							}
-						})
+						});
 					} else {
-						ingest = true
-						var startDate = new Date()
-						console.log('Empty db. Starting ingest cycle at: ' + startDate)
+						ingest = true;
+						var startDate = new Date();
+						console.log('Empty db. Starting ingest cycle at: ' + startDate);
 						dbSeedData('week',function(){
 							dbSeedData('month', function(){
 								dbSeedData('year', function(){
 									console.log(
-										'ingest cycle is done.'
-										+ '\nStarted: '+ startDate
-										+ '\nEnded: ' + Date()
+										'ingest cycle is done.' +
+										'\nStarted: '+ startDate +
+										'\nEnded: ' + Date()
 									);
-									ingest = false
-								})
-							})
-						})
+									ingest = false;
+								});
+							});
+						});
 						//res.status(500).send('Empty db. Return after ingest cycle is done.')
 						//retrieve previous available data to present
 						db.collection(dbConfig.collection).findOne({timeInterval: req.params.timeInterval, createDate: yesterday}, function(e, results, next){
-							if(e) res.status(500).send(e)
+							if(e) res.status(500).send(e);
 							if(results){
-								res.send(results.Data)
+								res.send(results.Data);
 							}
-						})
+						});
 				}
 			}
-		})
+		});
 	});
-
-	/* GET  users loyalty analytics data */
-	//TODO in production secure this route by using isAuthenticated param
-	router.get('/ga-data/visitor-type-data', function(req, res){
-
-		//get the parametrized partial query objects
-		var query = require('../analytics/AnalyticsQueries').visitorTypesQuery
-		//get the google-analytics authentication module
-		var googleAnalytics = require('../analytics/googleAnalytics2').getData;
-
-		googleAnalytics(function (err, result){
-			if(err) return console.log(err)
-			if(result){
-				//rename object key with the query key
-				var data = {}
-				data['visitorTypesQuery'] = result.rows
-				res.send(data)
-			}
-		}, query)
-	});
-
-	/* GET  google operating system related analytics data */
-	//TODO in production secure this route by using isAuthenticated param
-	router.get('/ga-data/visits-by-country-data', function(req, res){
-
-		//get the parametrized partial query objects
-		var query = require('../analytics/AnalyticsQueries').countryVisitsQuery
-		//get the google-analytics authentication module
-		var googleAnalytics = require('../analytics/googleAnalytics2').getData;
-
-		googleAnalytics(function (err, result){
-			if(err) return console.log(err)
-			if(result){
-				//rename object key with the query key
-				var data = {}
-				data['countryVisitsQuery'] = result.rows
-				res.send(data)
-			}
-		}, query)
-	});
-
-	/* GET  google operating system related analytics data */
-	//TODO in production secure this route by using isAuthenticated param
-	router.get('/ga-data/os-data', function(req, res){
-
-		//get the parametrized partial query objects
-		var query = require('../analytics/AnalyticsQueries').osQuery
-		//get the google-analytics authentication module
-		var googleAnalytics = require('../analytics/googleAnalytics2').getData;
-
-		googleAnalytics(function (err, result){
-
-			if(err) return console.log(err)
-			if(result){
-				//rename object key with the query key
-				var data = {}
-				data['osQuery'] = result.rows
-				res.send(data)
-			}
-		}, query)
-	});
-
-
-	/* GET  daily users analytics data */
-	//TODO in production secure this route by using isAuthenticated param
-	router.get('/ga-data/dayly-users-data', function(req, res){
-
-		//get the parametrized partial query objects
-		var query = require('../analytics/AnalyticsQueries').dailyUsersQuery
-		//get the google-analytics authentication module
-		var googleAnalytics = require('../analytics/googleAnalytics2').getData;
-
-		googleAnalytics(function (err, result){
-			if(err) return console.log(err)
-			if(result){
-				//rename object key with the query key
-				var data = {}
-				data['dailyUsersQuery'] = result.rows
-				res.send(data)
-			}
-		}, query)
-	});
-
-	/* GET  daily users analytics data */
-	//TODO in production secure this route by using isAuthenticated param
-	router.get('/ga-data/user-frequency-data', function(req, res){
-
-		//get the parametrized partial query objects
-		var query = require('../analytics/AnalyticsQueries').frequencyQuery
-		//get the google-analytics authentication module
-		var googleAnalytics = require('../analytics/googleAnalytics2').getData;
-
-		googleAnalytics(function (err, result){
-			if(err) return console.log(err)
-			if(result){
-				//rename object key with the query key
-				var data = {}
-				data['frequencyQuery'] = result.rows
-				res.send(data)
-			}
-		}, query)
-	});
-
-	/* GET  users loyalty analytics data */
-	//TODO in production secure this route by using isAuthenticated param
-	router.get('/ga-data/user-loyalty-data', function(req, res){
-
-		//get the parametrized partial query objects
-		var query = require('../analytics/AnalyticsQueries').loyaltyQuery
-		//get the google-analytics authentication module
-		var googleAnalytics = require('../analytics/googleAnalytics2').getData;
-
-		googleAnalytics(function (err, result){
-			if(err) return console.log(err)
-			if(result){
-				//rename object key with the query key
-				var data = {}
-				data['loyaltyQuery'] = result.rows
-				res.send(data)
-			}
-		}, query)
-	});
-
-
 
 	return router;
-}
-
-
-
-
-
+};
